1127dc2e2b6ef8733c4c6f13d5dc2badf0503921
AJ Heflin <me@ajheflin.com>
2021-09-16 15:04:41 -0400

    Finished module

---
+++ src/components/Users.js
@@ -0,0 +1,62 @@
+import {Component} from 'react';
+import User from './User';
+
+import classes from './Users.module.css';
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showUsers: true,
+    };
+  };
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if(this.props.users.length === 0) {
+      throw new Error('No users provided!');
+    }
+  }
+
+
+  toggleUsersHandler() {
+    this.setState((prevState) => {
+      return {showUsers: !prevState.showUsers};
+    });
+  }
+  render(props) {
+    const usersList = (
+        <ul>
+          {this.props.users.map((user) => (
+              <User key={user.id} name={user.name} />
+          ))}
+        </ul>
+    );
+    return (
+        <div className={classes.users}>
+          <button onClick={this.toggleUsersHandler.bind(this)}>
+            {this.state.showUsers ? 'Hide' : 'Show'} Users
+          </button>
+          {this.state.showUsers && usersList}
+        </div>
+    );
+  }
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+//
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+//
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
+
+export default Users;
